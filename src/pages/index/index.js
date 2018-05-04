3cc5acd064f98f09cc24282b2a8aa6582831449f
kun cheng <kuncheng@kun.local>
2018-05-04 18:00:03 +0800

    404 update

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,22 +1,52 @@
 import React, {Component} from 'react'
-
+import {Link} from 'react-router-dom'
 import './index.css'
 
 export default class Index extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            type: 1
-        }
-        this.changeTabe = (i) => {
-            console.log(i)
-            this.setState({type: i})
+            word: 'I MEET YOU , TREASURE YOU , LOVE YOU , EVEN , I MISS YOU ...',
+            current: 0,
+            timer: null
         }
     }
+
+    componentDidMount(){
+        const len = this.state.word.length
+        this.setState({
+            timer: setInterval(() => {
+                if (this.state.current == len) {
+                    this.setState({
+                        current: 0
+                    })
+                    return
+                }
+                this.setState({
+                    current: this.state.current + 1
+                })
+            },800)
+        })
+    }
     render() {
+        const list = this.state.word.split('')
         return (
             <div className="content">
-                hello
+                <div className="header">
+                    <div className="logo">
+                        <img className="logoimg" src={require('../../asset/images/logo.png')} alt=""/>
+                    </div>
+                    <p className="slogn">
+                        {
+                            list.map((item,i) => <span key={i} className={`muletter ${item == ' ' ? 'space' : ''} ${this.state.current == i ? 'current' : ''}`}>{item == ' ' ? '' : item}</span>)
+                        }
+                    </p>
+                    <div className="nav">
+                        <Link className="nav-item" to="/">Home</Link>
+                        <Link className="nav-item" to="/about">About</Link>
+                        <Link className="nav-item" to="/profiles">profiles</Link>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/router.js
+++ src/router.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {browserHistory} from 'react-router'
-import Bundle from './utils/bundles'
-import Index from 'bundle-loader?lazy&name=index!@/index'
 
-const createComponent = (Com) => (props) => (
-    <Bundle load={Com}>
-        {(Com) => Com
-            ? <Com {...props} />
-            : ''
-}
-    </Bundle>
-)
+import BaseLayout from 'bundle-loader?lazy&name=index!@/index'
+import { createComponent } from './utils/utils'
+// const createComponent = (Com) => (props) => (
+//     <Bundle load={Com}>
+//         {(Com) => Com
+//             ? <Com {...props} />
+//             : ''
+// }
+//     </Bundle>
+// )
 
 const getRouter = () => {
     return (
         <Router history={browserHistory}>
             <div>
-                <Route path="/" component={createComponent(Index)}/>
+                <Route path="/" component={createComponent(BaseLayout)}/>
             </div>
         </Router>
     )
