4774ee48be0bf9e66206bdcc3eeda4c632419b3b
kun cheng <kuncheng@kun.local>
2018-06-14 08:00:15 +0800

    update

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -7,25 +7,38 @@ export default class Index extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            list: []
         }
+
+
+    }
+    componentWillMount(){
+        fetch('/api/getlist').then(res => res.json()).then(res => {
+            this.setState({
+                list: res.data
+            })
+        })
     }
     render() {
         return (
             <div>
                 <div className="left">
                     <div className="articles">
-                        <div className="art-item">
-                            <Link to="/">
-                                <h3 className="art-title">
-                                    饿了么PWA升级实践
-                                </h3>
-                                <p className="art-desc">今年 9 月份的时候，《程序员》杂志社就邀请我写一篇关于 PWA 的文章。后来花式拖稿，拖过了 10 月的 QCon，11 月的 GDG DevFest，终于在 12 月把这篇长文熬了出来。几次分享的不成熟，这次的结构算是比较满意了。「 可能是目前中文世界里对 PWA 最全面详细的长文了」，希望你能喜欢。
-                              本文首发于 CSDN 与《程序员》2017 年 2 月刊，同步发布于 Hux B...</p>
-                            </Link>
-                            <p className="pubdate">
-                                published by ck on june 24,2017
+                        {
+                            this.state.list.map(item => (
+                                <div className="art-item" key={item.id}>
+                                    <Link to="/">
+                                        <h3 className="art-title">
+                                            {item.title}
+                                        </h3>
+                                        <p className="art-desc">{item.desc}</p>
+                                    </Link>
+                                    <p className="pubdate">
+                                        published by {item.author} on {item.create_time}
                             </p>
-                        </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
                 <div className="right">
--- webpack.dev.js
+++ webpack.dev.js
@@ -27,7 +27,7 @@ const devConfig = {
         // progress: true,   cli only 需要在命令行配置
         proxy:{
             '/api/':{
-                target: 'http://wandou.incubate.os',
+                target: 'http://localhost:8080',
                 changeOrigin: true
             }
         }
